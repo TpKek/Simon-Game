469b594b5817a0bca5b3432b902f34fba01bfff7
TpK <Bertin.Dreyer1@gmail.com>
2026-02-05 12:01:17 +0200

    Update game.js

--- game.js
+++ game.js
@@ -3,26 +3,43 @@
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
+var canClick = true;
+
+$(document).keypress(function () {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
 
 $('.btn').click(function () {
-  // console.log('Button Clicked');
-  var userChosenColour = $(this).attr('id');
-  // console.log(userChosenColour);
-  userClickedPattern.push(userChosenColour);
-  // console.log(userClickedPattern);
-  playSound(userChosenColour);
+  if (started && canClick) {
+    // console.log('Button Clicked');w
+    var userChosenColour = $(this).attr('id');
+    // console.log(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+  }
 });
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  userClickedPattern = [];
 
   $('#' + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100);
 
   playSound(randomChosenColour);
+  level++;
+  $('#level-title').text('Level' + ' ' + level);
 }
 
 function playSound(name) {
@@ -49,10 +66,58 @@ function playSound(name) {
       yellowAudio.play();
       break;
 
+    case 'wrong':
+      var wrongAudio = new Audio('sounds/wrong.mp3');
+      wrongAudio.play();
+      break;
+
     default:
       console.log('No sound found for: ' + name);
       break;
   }
 }
 
-nextSequence();
+function animatePress(currentColour) {
+  // console.log(currentColour);
+
+  $('#' + currentColour).addClass('pressed');
+
+  setTimeout(function () {
+    $('#' + currentColour).removeClass('pressed');
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  // console.log(gamePattern);
+  // console.log(userClickedPattern);
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    canClick = false;
+    // console.log('success');
+
+    if (userClickedPattern.length === gamePattern.length) {
+      // 3. Call nextSequence() after a 1000ms delay
+      setTimeout(function () {
+        nextSequence();
+        canClick = true;
+      }, 1000);
+    } else {
+      canClick = true;
+    }
+  } else {
+    // console.log('failure');
+    playSound('wrong');
+    $('h1').text('Game Over, Press Any Key to Restart');
+    $('body').addClass('game-over');
+    setTimeout(function () {
+      $('body').removeClass('game-over');
+    }, 200);
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+}
